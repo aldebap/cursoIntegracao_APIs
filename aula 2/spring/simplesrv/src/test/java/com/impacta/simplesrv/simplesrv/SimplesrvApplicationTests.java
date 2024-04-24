d18ea85d2b8425f04e75f156fa7b423f31e73595
aldebap <aldeba_bla@yahoo.com>
2024-04-24 18:47:02 -0300

    Reorganização dos projetos da aula 2

---
+++ aula 2/spring/simplesrv/src/test/java/com/impacta/simplesrv/simplesrv/SimplesrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impacta.simplesrv.simplesrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
